fa9ceba298e1f5d9dadb57237583b5167a712dd5
Jabriel Millen Andrade <jandrade2018@Room208-15.local>
2017-03-30 14:13:57 -0700

    buttoncommit

---
+++ ARHSFinal/CharacterPage.swift
@@ -0,0 +1,9 @@
+//
+//  CharacterPage.swift
+//  ARHSFinal
+//
+//  Created by Jabriel Millen Andrade on 3/30/17.
+//  Copyright © 2017 Jabriel Millen Andrade. All rights reserved.
+//
+
+import Foundation
